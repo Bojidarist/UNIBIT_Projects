b234051c343c43fffe2fec04db96b7e6701d71f0
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-10-28 18:12:41 +0200

    Add NumberSum to PBCOH
    Add NumberSum to ProgrammingBasicsCourseOneHomework.

---
+++ CourseOne/ProgrammingBasics/ProgrammingBasicsCourseOneHomework/NumberSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NumberSum
+{
+    /// <summary>
+    /// Напишете програма, която въвежда цяло число num и отпечатва сумата от цифрите му.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            // Ask the user for input
+            Console.Write("Type a number: ");
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            // Do the calculations
+            for (int i = 0; i < input.Length; i++)
+            {
+                int number = int.Parse(input[i].ToString());
+                if (i == input.Length - 1)
+                {
+                    Console.Write($"{ number }");
+                }
+                else
+                {
+                    Console.Write($"{ number } + ");
+                }
+                sum += number;
+            }
+
+            // Print the sum to the console
+            Console.Write($" = { sum }");
+
+            // Wait for input so the program does not close
+            Console.WriteLine("\nPress Any Key To Exit . . .");
+            Console.ReadKey();
+        }
+    }
+}
